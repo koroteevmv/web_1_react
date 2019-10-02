bf2f31c3943c03450ee9ad658ec0f70c3c24b2e8
koroteevmv <koroteevmv@gmail.com>
2019-10-02 13:36:41 +0300

    Adding a state and passing as a prop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React from 'react';
 import './App.css';
 import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <Todos></Todos>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    todos:[
+      {
+        id: 1,
+        title: 'Вынести мусор',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Встреча с друзьями',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Совещание на работе',
+        completed: false,
+      },
+    ]
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <Todos todos={this.state.todos}/>
+        </div>
+    );
+  }
 }
 
 export default App;
