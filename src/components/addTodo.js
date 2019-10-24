22fa04a967b8857a033ff7ea7a664668faf9e0ef
BogdanGonchar123 <48789363+BogdanGonchar123@users.noreply.github.com>
2019-10-24 13:48:16 +0300

    olololo

--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,22 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class AddTodo extends Component {
-    render() {
-        return (
-            <form style={{display: 'flex'}}>
-                <input type='text'
-                       name='title'
-                       placeholder='Add todo'
-                       style={{flex: '10'}}
-                />
-                <input type='submit'
-                       value='Submit'
-                       className='btm'
-                       style={{flex: '1'}}
-                />
-            </form>
-        );
-    }
+  state = {
+    title: ""
+  };
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  render() {
+    return (
+      <form style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Add todo"
+          style={{ flex: "10" }}
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+
+        <input
+          type="submit"
+          value="Submit"
+          className="btm"
+          style={{ flex: "1" }}
+        />
+      </form>
+    );
+  }
 }
 
 export default AddTodo;
