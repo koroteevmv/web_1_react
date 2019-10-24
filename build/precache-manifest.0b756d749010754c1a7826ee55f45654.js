b62a5a34824e4c855236cd1a181a25d043e5022b
glvnvpo <47975328+glvnvpo@users.noreply.github.com>
2019-10-24 16:09:36 +0300

    Add files via upload

---
+++ build/precache-manifest.0b756d749010754c1a7826ee55f45654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a9c7b132138cb4f298cdf9cdc7236f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1379e6bd2e3f1beadc1b",
+    "url": "/static/css/main.cecea99f.chunk.css"
+  },
+  {
+    "revision": "2e26ed8b92f51680e60d",
+    "url": "/static/js/2.615b08d1.chunk.js"
+  },
+  {
+    "revision": "1379e6bd2e3f1beadc1b",
+    "url": "/static/js/main.94aed83e.chunk.js"
+  },
+  {
+    "revision": "c68001c7d63e1d6a705d",
+    "url": "/static/js/runtime-main.06cb610f.js"
+  },
+  {
+    "revision": "09b1335b31a952135ae5b603a4c86083",
+    "url": "/static/media/ad1.09b1335b.png"
+  }
+]);
