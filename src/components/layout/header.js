d782f0bde5584e4b3f1e41877ef21bdf921c7b55
DoctorVrach <ern.no@mail.ru>
2019-10-28 12:30:59 +0300

    Все задания

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={LinkStyle} to="/">На главную </Link> |
+            <Link style={LinkStyle} to="/about"> О приложении
+            </Link>
+
         </header>
     )
 }
-
+const LinkStyle = {
+    color: '#fff',
+    textDecoration:'none',
+};
 const headerStyle = {
     background: '#333',
     color: '#fff',
