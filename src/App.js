ba8bb13a4c74e60d34f42c563543b81b6f27bf65
Нгуен Кхань Нам <knambusiness@gmail.com>
2019-10-30 18:23:41 +0300

    Нгуен Кхань Нам Пи4-1

--- src/App.js
+++ src/App.js
@@ -3,55 +3,143 @@ import './App.css';
 import Todos from './components/Todos'
 import Header from "./components/layout/header";
 import AddTodo from "./components/addTodo";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from "./components/About";
+import Filtr from "./components/Filtr"
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: '1',
+        completed: false,
+        duration: 6,
+      },
+      {
+        id: 2,
+        title: '2',
+        completed: true,
+        duration: 4,
+      },
+      {
+        id: 3,
+        title: '3',
+        completed: false,
+        duration: 2,
+      },
+    ]
+  };
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
 
-class App extends React.Component{
+
+
+
+  addTodo = (title, duration) => {
+    if (title === '' | duration === '') {
+      alert('Введите дело')
+    }
+    else{
+      if (duration <= 0){
+        alert('Введите число >0')
+      }
+      else{
+        const len = this.state.todos.length;
+        const newTodo = {
+          id: len + 1,
+          title: title,
+          duration: duration,
+          completed: false
+        };
+        this.setState({todos: [...this.state.todos, newTodo ].sort( (a,b) => a.duration > b.duration ? 1: -1 ) },
+            () => {this.todosCopy = this.state.todos; localStorage.setItem('List', JSON.stringify(this.state.todos));})
+      }
+    }
+  };
+
+  componentWillMount() {
+    if (localStorage.getItem('Dela')) {
+      this.setState({
+        todos: JSON.parse(localStorage.getItem('Dela'))
+      })
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem('Dela', JSON.stringify(nextState.todos));
+  }
+
+  delTodo = (id) => {
+    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+  }
   state = {
     todos:[
       {
         id: 1,
-        title: 'Вынести мусор',
+        title: '1',
+        duration: 6,
         completed: false,
       },
       {
         id: 2,
-        title: 'Встреча с друзьями',
-        completed: true,
+        duration: 4,
+        title: '2',
+        completed: false,
       },
       {
         id: 3,
-        title: 'Совещание на работе',
+        duration: 2,
+        title: '3',
         completed: false,
       },
     ]
   };
-  // Toggle complete
-  markComplete =(id) => {
-    this.setState({ todos: this.state.todos.map(todo =>{
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-      }) });
+  Fil = false;
+  FiltrTodo = (title) => {
+    if(title !== ''){
+
+      this.todosCopy = this.state.todos;
+      this.Fil = true;
+      this.setState({todos: this.state.todos.filter(todo => todo.title === title)},
+          () => console.log(this.state.todos));}
+
   };
 
-  delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id != id)]});
-  }
-
+  delFiltr = () => {
+    if(this.Fil === true){
+      this.setState({todos:this.todosCopy})
+    }
+  };
   render() {
     return (
-        <div className="App">
-          <div className="container">
-            <Header/>
-            <AddTodo/>
-            <Todos todos={this.state.todos}
-                   markComplete={this.markComplete}
-                   delTodo={this.delTodo}
-            />
+        <Router>
+          <div className="App">
+            <div className="container">
+              <Header/>
+              <Route exact path='/' render={props => (
+                  <React.Fragment>
+
+                    <AddTodo addTodo={this.addTodo}/>
+                    <Filtr FiltrTodo = {this.FiltrTodo} delFiltr = {this.delFiltr}/>
+                    <Todos todos={this.state.todos}
+                           markComplete={this.markComplete}
+                           delTodo={this.delTodo}/>
+                  </React.Fragment>
+              )}/>
+              <Route path='/about'
+                     component={About}/>
+            </div>
           </div>
-        </div>
+        </Router>
     );
   }
 }
-
 export default App;
