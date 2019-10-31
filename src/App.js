9529326f51a4ca7bf530c1cab8401c0c1b7b39da
andreyanov <48549869+andreyanov@users.noreply.github.com>
2019-10-31 16:44:44 +0300

    ivan

--- src/App.js
+++ src/App.js
@@ -1,55 +1,97 @@
-import React from 'react';
-import './App.css';
-import Todos from './components/Todos'
+import React from "react";
+import "./App.css";
+import Todos from "./components/Todos";
 import Header from "./components/layout/header";
 import AddTodo from "./components/addTodo";
-
-class App extends React.Component{
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./components/About";
+class App extends React.Component {
   state = {
-    todos:[
+    todos: [
       {
         id: 1,
-        title: 'Вынести мусор',
-        completed: false,
+        title: "Вынести мусор",
+        completed: false
       },
       {
         id: 2,
-        title: 'Встреча с друзьями',
-        completed: true,
+        title: "Встреча с друзьями",
+        completed: false
       },
       {
         id: 3,
-        title: 'Совещание на работе',
-        completed: false,
-      },
+        title: "Совещание на работе",
+        completed: false
+      }
     ]
   };
   // Toggle complete
-  markComplete =(id) => {
-    this.setState({ todos: this.state.todos.map(todo =>{
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-      }) });
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
   };
 
-  delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id != id)]});
-  }
+  addTodo = (title, duration) => {
+    if ((title === "") | (duration === "")) {
+      alert("Нужно ввести дело и срок");
+    } else {
+      const len = this.state.todos.length;
+      const newTodo = {
+        id: len + 1,
+        title: title,
+        duration: duration,
+        completed: false
+      };
+      this.setState(
+        {
+          todos: [...this.state.todos, newTodo].sort((a, b) =>
+            a.duration > b.duration ? 1 : -1
+          )
+        },
+        () => {
+          this.todosCopy = this.state.todos;
+          localStorage.setItem("List", JSON.stringify(this.state.todos));
+        }
+      );
+    }
+  };
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
 
   render() {
     return (
+      <Router>
         <div className="App">
           <div className="container">
-            <Header/>
-            <AddTodo/>
-            <Todos todos={this.state.todos}
-                   markComplete={this.markComplete}
-                   delTodo={this.delTodo}
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
             />
+            <Route path="/about" component={About} />
           </div>
         </div>
+      </Router>
     );
   }
 }
