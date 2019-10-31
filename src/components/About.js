fdb51b6e52ae03113208918502a34d895d010498
ars322 <arseny2033@mail.ru>
2019-10-31 16:31:40 +0300

    123

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+class About extends Component {
+  render() {
+    return <React.Fragment>ы</React.Fragment>;
+  }
+}
+export default About;
--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,22 +1,53 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class AddTodo extends Component {
-    render() {
-        return (
-            <form style={{display: 'flex'}}>
-                <input type='text'
-                       name='title'
-                       placeholder='Add todo'
-                       style={{flex: '10'}}
-                />
-                <input type='submit'
-                       value='Submit'
-                       className='btm'
-                       style={{flex: '1'}}
-                />
-            </form>
-        );
-    }
+  state = {
+    title: "",
+    duration: ""
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title, ", Срок: " + this.state.duration);
+    this.setState({ title: "", duration: "" });
+  };
+
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Добавить дело"
+            style={{ flex: "1" }}
+            value={this.state.title}
+            onChange={this.onChange}
+          />
+          <input
+            type="number"
+            name="duration"
+            placeholder="Срок"
+            style={{ flex: "1" }}
+            value={this.state.duration}
+            onChange={this.onChange}
+          />
+        </form>
+        <form onSubmit={this.onSubmit} style={{ display: "flex" }}>
+          <input
+            type="submit"
+            value="Добавить"
+            class="btn btn-primary"
+            style={{ flex: "1" }}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddTodo;
