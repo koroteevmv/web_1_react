e042ea8ff9648ec0f56ab4c68b288f6127734154
Prokazniik <illidan.mlg@gmail.com>
2019-10-31 15:13:56 +0300

    5 заданий

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react';
+class About extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>About this app</h1>
+                <p>This application is intended for the introduction of cases. Developer Romashka corp.
+
+                </p>
+            </React.Fragment>
+        );
+    }
+}
+export default About;
